1d3ceb3cce23a7c87e256ceebc31bec4aeb11515
merchfck <logistikman@bk.ru>
2023-01-09 00:57:56 +0300

    first commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+bot_name = "Basedgebot"  # choose your twitch nickname
+token = "paste your token"
+initial_channels = ["channel1", "channel2"]
+
+prefix = '!'  # set up your prefix for default commands
+secondary_prefix = '@'  # set up your secondary prefix for admin commands #
+
+postgres_host = "localhost"
+postgres_user = "postgres"
+postgres_password = "paste your postgres password"
+postgres_title = "postgres"
